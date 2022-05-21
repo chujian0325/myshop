cb8dd71f4914055bee80f698f776a95be4033a86
chujian0325 <653510854@qq.com>
2022-05-21 16:42:40 +0800

    轮播

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -6,8 +6,23 @@
         <input type="text" placeholder="搜索">
       </div>
     </div>
+    <!-- 轮播图 -->
+    <swiper :indicator-dots="true">
+      <swiper-item v-for="(item, index) in imgList" :key="index">
+        <image :src="item" />
+      </swiper-item>
+    </swiper>
   </div>
 </template>
+<script>
+export default {
+  data () {
+    return {
+      imgList: []
+    }
+  }
+}
+</script>
 <style scoped>
 .search-bar {
   background-color: #fe2d4a;
