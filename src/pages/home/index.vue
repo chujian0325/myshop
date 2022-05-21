c1a89bab16b2a869b5a6b2fe182fdaca32a92beb
chujian0325 <653510854@qq.com>
2022-05-21 16:24:32 +0800

    全局导航栏背景色、home页搜索

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,5 +1,20 @@
 <template>
   <div>
-    首页
+    <!-- 搜索条 -->
+    <div class="search-bar">
+      <div class="search-input">
+        <input type="text" placeholder="搜索">
+      </div>
+    </div>
   </div>
 </template>
+<style scoped>
+.search-bar {
+  background-color: #fe2d4a;
+  padding: 20rpx;
+}
+.search-input {
+  background-color: #fff;
+  text-align: center;
+}
+</style>
